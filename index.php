d0ef37573a4d045236952bc88ec293fd99711ee9
demerll <58672347+demerll@users.noreply.github.com>
2020-02-02 18:03:16 -0600

    Completed Assignment

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Assignment 7</title>
+
+     <!--Index.php
+    Assignment 7
+    INFO 2340 
+    Donjeta Emerllahu-->
+
+</head>
+<body>
+    <?php //define variables here.
+    $my_dinner= 'Pizza';
+    $_num = 2;
+    $fav_place='Joslyn art museum';
+    $fav_animal='lion';
+
+    ?>
+    <h1>Good morning, Nebraska</h1>
+    <div>Last night I ate <?php Echo "$my_dinner";?>.</div>
+    <div>While it was good, it wasn't perfect. I'd give it <?php Echo "$_num";?> stars out of 4.</div>
+    <div>Tomorrow I plan to go to the <?php Echo "$fav_place";?>.</div>
+    <div>My favorite animal is the <?php Echo "$fav_animal";?>.</div>
+</body>
+</html>
